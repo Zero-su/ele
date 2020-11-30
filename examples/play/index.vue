31e9eaea41272c31b1ad62521dd115bfae2788f1
Shan Shan2 Su <sushan2@Lenovo.com>
2020-11-30 13:53:43 +0800

    记录

---
+++ examples/play/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="margin: 20px;">
+    <el-date-picker
+      v-model="value1"
+      type="datetimerange"
+      format="yyyy-MM-dd HH:mm"
+      :isDivide="true"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期">
+    </el-date-picker>
+
+    <el-date-picker
+      v-model="value4"
+      type="datetimerange"
+      format="yyyy-MM-dd HH:mm"
+      :isDivide="false"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期">
+    </el-date-picker>
+    
+    <el-time-picker
+    v-model="value2"
+    :isDivide="true"
+    format="HH:mm"
+    placeholder="任意时间点">
+    </el-time-picker>
+
+    <el-time-picker
+    v-model="value3"
+    :isDivide="false"
+    format="HH:mm"
+    placeholder="任意时间点">
+  </el-time-picker>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value1: [],
+        value4: [],
+        value2: '',
+        value3: '',
+        input: 'Hello Element UI!'
+      };
+    }
+  };
+</script>
